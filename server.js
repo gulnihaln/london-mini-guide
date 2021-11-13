30cabf4c91a4dcbe0ad5ea6de8b5a5776b87dcf1
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-13 21:09:46 +0000

    feat: added express and read data

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+const heathrow = require("./data/Heathrow.json");
+const harrow = require("./data/Harrow.json");
+const stratford = require("./data/Stratford.json");
+
+app.get("/:city/:category", function (request, response) {
+  const city = request.params.city;
+  const upCity = city[0].toUpperCase() + city.substring(1);
+  const category = request.params.category;
+  const file = require(`./data/${upCity}.json`);
+  response.send(file[category]);
+
 });
 
 app.listen(3000);
